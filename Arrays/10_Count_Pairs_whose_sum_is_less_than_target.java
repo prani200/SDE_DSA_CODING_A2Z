5a3b3ea1aa84763343d7860f97b30756539cecaf
Prani Sahith Kummari <pranisahith@gmail.com>
2025-10-13 23:02:42 +0530

    Create 10_Count_Pairs_whose_sum_is_less_than_target.java

---
+++ Arrays/10_Count_Pairs_whose_sum_is_less_than_target.java
@@ -0,0 +1,33 @@
+
+class Solution 
+{
+    int countPairs(int arr[], int target) 
+    {
+        Arrays.sort(arr);
+        int left = 0;
+        int right = arr.length-1;
+        int cnt=0;
+        
+        while(left<right)
+        {
+            
+            int sum = arr[left]+arr[right];
+            
+            if(sum < target)
+            {
+               cnt+=right-left;
+               left++;
+            }
+            else{
+                 right--;
+            }
+ 
+        }
+        
+        return cnt;
+        
+        
+        
+        
+    }
+}
